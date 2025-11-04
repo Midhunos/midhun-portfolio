b7b8cf93ab4e1d906f5bca0c00a7ac7e79984925
Midhunos <midhunos24@gmail.com>
2025-11-04 11:03:15 +0530

    Add email functionality to contact form and enhance UI

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -2,8 +2,6 @@ import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/midhuntwo.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
 import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
@@ -15,7 +13,6 @@ function Hero() {
 
   const { theme,toggleTheme } = useTheme();
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -33,17 +30,14 @@ function Hero() {
         <h1>midhun os</h1>
         <h2>FullStack Developer</h2>
         <span>
-            <a href="http://twitter.com" target="_blank">
-            <img src={twitterIcon} alt="twitter"></img>
-            </a>
-            <a href="http://github.com" target="_blank">
+            <a href="https://github.com/Midhunos" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon} alt="Github"></img>
             </a>
-            <a href="https://www.linkedin.com/in/midhun-os/" target="_blank">
+            <a href="https://www.linkedin.com/in/midhun-os/" target="_blank" rel="noopener noreferrer">
             <img src={linkedinIcon} alt="Linkedin"></img>
             </a>
         </span>
-        <p className={styles.description}>With a passion for  developing modern react web apps for commercial businesses </p>
+        <p className={styles.description}>With a passion for developing modern Python applications and web solutions for commercial businesses</p>
         <a href={CV} download>
           <button className="hover">
             Resume
